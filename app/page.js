ba28db8a849524f91a23f54ab6c50b447d1a992b
Muhammed Adil S <muhammed.adil@qubiqon.io>
2025-10-24 18:01:52 +0530

    Added footer section

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">Welcome to My Next.js App 🚀</h1>
+        <p className="text-gray-500">Learning Git branches step by step</p>
+        <footer className="mt-10 text-sm text-gray-400">
+          © 2025 My Next.js App
+        </footer>
+      </main>
+    )
+  }
+  
